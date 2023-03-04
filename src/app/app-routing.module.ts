447edb73d355d53817af81c2c0acb4fbf49c7e8a
Biagio Ruotolo <biagio.ruotolo@aboutyou.com>
2023-03-04 04:21:29 +0100

    feat: adds graphql configs and env files

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,26 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+/**
+ * The routes are lazy loaded, so the app doesn't load all the modules at once.
+ * This is a good practice for performance.
+ */
+const routes: Routes = [
+  {
+    path: 'feed',
+    loadChildren: () =>
+      import('./features/feed/feed.module').then(m => m.FeedModule),
+  },
+  {
+    path: 'post',
+    loadChildren: () =>
+      import('./features/post/post.module').then(m => m.PostModule),
+  },
+  { path: '**', redirectTo: 'feed' },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
